cc2587fcb455c1597c0e3247892f354e0aae2232
Nasief Khan <nasiefk14@gmail.com>
2023-03-26 00:29:36 +0200

    Navbar Setup

--- Portfolio/src/components/Navbar.tsx
+++ Portfolio/src/components/Navbar.tsx
@@ -1,22 +1,32 @@
 import logo from '../assets/logo.png'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
-  return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#5CDB95] text-[#05386B]'>
-        <div>
-            <img src={logo} alt="Home Logo" className='HomeLogo'/>
-        </div>
-        <div>
-            <ul>
+    return (
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#5CDB95] text-[#05386B]'>
+            <div>
+                <img src={logo} alt="Home Logo" className='HomeLogo' />
+            </div>
+            <div>
+                <ul className='flex'>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Skils</li>
+                    <li>Work</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='hidden'>
+                <FaBars />
+            </div>
+            <ul className='hidden'>
             <li>Home</li>
-            <li>About</li>
-            <li>Skils</li>
-            <li>Work</li>
-            <li>Contact</li>
-        </ul>
+                    <li>About</li>
+                    <li>Skils</li>
+                    <li>Work</li>
+                    <li>Contact</li>
+            </ul>
         </div>
-    </div>
     )
 };
 
