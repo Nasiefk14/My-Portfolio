14de96d193f3fd0f991ecc37ae9c1051eab9718d
Nasief Khan <nasiefk14@gmail.com>
2023-03-28 00:18:48 +0200

    Navbar Setup

--- Portfolio/src/components/Navbar.tsx
+++ Portfolio/src/components/Navbar.tsx
@@ -1,33 +1,37 @@
-import logo from '../assets/logo.png'
-import { FaBars, FaTimes } from 'react-icons/fa'
+
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-    return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#5CDB95] text-[#05386B]'>
-            <div>
-                <img src={logo} alt="Home Logo" className='HomeLogo' />
-            </div>
-            <div>
-                <ul className='flex'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Skils</li>
-                    <li>Work</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <div className='hidden'>
-                <FaBars />
-            </div>
-            <ul className='hidden'>
-            <li>Home</li>
-                    <li>About</li>
-                    <li>Skils</li>
-                    <li>Work</li>
-                    <li>Contact</li>
-            </ul>
-        </div>
-    )
+    const [nav, setNav] = useState<boolean>(false);
+    const handleClick = () => setNav(!nav)
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#5CDB95] text-[#05386B]">
+      <div>
+        <img src={logo} alt="Home Logo" className="HomeLogo" />
+      </div>
+      <ul className="hidden md:flex">
+        <li>About</li>
+        <li>Experience</li>
+        <li>Work</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars size={15}/> : <FaTimes />}
+      </div>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#5CDB95] flex flex-col justify-center items-center'}>
+        <li className="py-5 text-4xl">About</li>
+        <li className="py-5 text-4xl">Experience</li>
+        <li className="py-5 text-4xl">Work</li>
+        <li className="py-5 text-4xl">Contact</li>
+        <li className="py-5 text-4xl">Resume</li>
+      </ul>
+      <div className="hidden"></div>
+    </div>
+  );
 };
 
 export default Navbar;
