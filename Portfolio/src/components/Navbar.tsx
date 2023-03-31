77350d0ae32f378a513d8ceca70a5a7012dbd20e
Nasief Khan <nasiefk14@gmail.com>
2023-03-31 11:05:58 +0200

    Trying to fix Vercel Build Issue

--- Portfolio/src/components/Navbar.tsx
+++ Portfolio/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from '../assets/Logo.png';
+import Logo from "../assets/Logo.jpeg";
 import { Spin as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
